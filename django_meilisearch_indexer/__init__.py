74edce4b84225db3b8219d827dd5dd4ccd6724f9
Jordan Kowal <kowaljordan@gmail.com>
2024-11-01 21:32:01 +0100

    chore: limit module exports

---
+++ django_meilisearch_indexer/__init__.py
@@ -0,0 +1,31 @@
+from .model_indexer import MeilisearchModelIndexer
+from .types import (
+    Faceting,
+    MeilisearchFilters,
+    MeilisearchFilterValue,
+    MeilisearchSearchHits,
+    MeilisearchSearchParameters,
+    MeilisearchSearchResults,
+    MeilisearchSettings,
+    MinWordSizeForTypos,
+    Pagination,
+    Precision,
+    RankingRule,
+    TypoTolerance,
+)
+
+__all__ = [
+    "MeilisearchModelIndexer",
+    "Faceting",
+    "MeilisearchFilters",
+    "MeilisearchFilterValue",
+    "MeilisearchSearchHits",
+    "MeilisearchSearchParameters",
+    "MeilisearchSearchResults",
+    "MeilisearchSettings",
+    "MinWordSizeForTypos",
+    "Pagination",
+    "Precision",
+    "RankingRule",
+    "TypoTolerance",
+]
